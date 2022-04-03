dd68fa98ac5b6d3860fc55440469acd8fef4aaaf
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-03 23:06:13 +0600

    set route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+      </Routes>
     </div>
   );
 }
