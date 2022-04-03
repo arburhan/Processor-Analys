b3340eb57482f36b266edcdf0f6de6f1e9394774
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-03 23:42:09 +0600

    not found added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './Components/Dashboard/Dashboard';
 import Reviews from './Components/Reviews/Reviews';
 import Blogs from './Components/Blogs/Blogs';
 import Header from './Components/Header/Header';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Result Not found</h1>
+            <img width={800} height={400} src="./images/resultNotFound.gif" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
