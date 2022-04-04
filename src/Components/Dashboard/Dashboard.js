3366543fdb88c910c6956ec1452770622214faf7
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-04 07:31:09 +0600

    bar chart added

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { PureComponent } from 'react';
-import { Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css';
 
 const Dashboard = () => {
     const [chartsData, setChartsData] = useState([]);
@@ -11,17 +11,44 @@ const Dashboard = () => {
     }, [])
     console.log(chartsData);
     return (
-        <div>
-            <h2>This is Dashboard</h2>
-            <LineChart width={600} height={350} data={chartsData}>
-                <Line type={'monotone'} dataKey='revenue'></Line>
-                <Line type={'monotone'} dataKey='sell' stroke="#82ca9d"></Line>
-                <XAxis dataKey="month"></XAxis>
-                <YAxis dataKey="investment"></YAxis>
-                <Tooltip />
-                <Legend verticalAlign="top" height={36} />
-            </LineChart>
-
+        <div className='dashboard heading'>
+            <h2>Dashboard</h2>
+            <div className='parent-charts'>
+                <div>
+                    <h3 className='heading'>All reports</h3>
+                    <LineChart width={600} height={350} data={chartsData}>
+                        <Line type={'monotone'} dataKey='revenue'></Line>
+                        <Line type={'monotone'} dataKey='sell' stroke="#82ca9d"></Line>
+                        <XAxis dataKey="month"></XAxis>
+                        <YAxis dataKey="investment"></YAxis>
+                        <Tooltip />
+                        <Legend verticalAlign="top" height={36} />
+                    </LineChart>
+                </div>
+                <div>
+                    <h3 className='heading'>Selling report</h3>
+                    <BarChart
+                        width={600}
+                        height={350}
+                        data={chartsData}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                        barSize={20}
+                    >
+                        <XAxis dataKey="month" scale="point" padding={{ left: 25, right: 10 }} />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend verticalAlign="top" height={36} />
+                        <CartesianGrid strokeDasharray="3 3" />
+                        {/* <Bar dataKey="revenue" fill="#8884d8" /> */}
+                        <Bar dataKey="sell" fill="#82ca9d" />
+                    </BarChart>
+                </div>
+            </div>
         </div>
     );
 };
