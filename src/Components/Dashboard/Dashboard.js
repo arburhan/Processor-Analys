946d71b2488152f1121230fa4a55f634c69df814
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-04 06:30:02 +0600

    line chart added

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { PureComponent } from 'react';
+import { Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+    const [chartsData, setChartsData] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setChartsData(data))
+    }, [])
+    console.log(chartsData);
     return (
         <div>
             <h2>This is Dashboard</h2>
+            <LineChart width={600} height={350} data={chartsData}>
+                <Line type={'monotone'} dataKey='revenue'></Line>
+                <Line type={'monotone'} dataKey='sell' stroke="#82ca9d"></Line>
+                <XAxis dataKey="month"></XAxis>
+                <YAxis dataKey="investment"></YAxis>
+                <Tooltip />
+                <Legend verticalAlign="top" height={36} />
+            </LineChart>
+
         </div>
     );
 };
