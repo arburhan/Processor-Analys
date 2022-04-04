7d8f168e07dcf0087fda8913709d89ace746a283
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-04 16:40:38 +0600

    reviews data added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -13,3 +13,17 @@ const Blogs = () => {
 <i class="bi bi-star-half"></i>
 */
 export default Blogs;
+
+
+/* 
+review
+
+#pros
+"Reasonably priced with great value. Great, lightweight technology and good support from partner."
+"It is also more affordable than most similar tools. The company behind the technology has been a pleasure to work with and is very responsive to our questions."
+"I love that our website visitors can now generate a nice, shareable pdf file off of content from the site."
+"Overall we are really happy. Anytime we have questions, the support via chat helps us out within minutes.
+
+#cons
+"The only thing I have found difficult is the alignment of components in the template builder."
+*/
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Reviews = () => {
+    const [reviews, setREviews] = useState([]);
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => setREviews(data))
+    }, [])
     return (
         <div>
             <h2>Review page</h2>
+            {
+                reviews.map(review => console.log(review))
+            }
         </div>
     );
 };
