ceca0ac4c30c8fb79bc72f0f5025934ed724053c
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-05 16:26:59 +0600

    style added on navbar

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,10 +4,10 @@ const About = () => {
     return (
         <div>
             <h1 className='text-center my-5' style={{ color: 'blueviolet' }}>Few line about us</h1>
-            <h3 className='px-5'>This is the Processor Analysis website. We have tried our best to give a review of the best, best quality and fastest processor in the market. In this case we have chosen the processor of amd.
+            <p className='px-5 fs-4'>This is the Processor Analysis website. We have tried our best to give a review of the best, best quality and fastest processor in the market. In this case we have chosen the processor of amd.
                 We may not match your preferences. Don't be sad.
                 Thank you for visiting us here.
-                Request for advice: <a href=""></a> </h3>
+                Request for advice: </p>
         </div>
     );
 };
