09ba2319f7f1d52acfe53061a5c06dcea7bace57
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-05 16:29:17 +0600

    style added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1 className='text-center my-5' style={{ color: 'blueviolet' }}>Few line about us</h1>
+            <p className='text-center my-5 fs-1' style={{ color: 'blueviolet' }}>Few line about us</p>
             <p className='px-5 fs-4'>This is the Processor Analysis website. We have tried our best to give a review of the best, best quality and fastest processor in the market. In this case we have chosen the processor of amd.
                 We may not match your preferences. Don't be sad.
                 Thank you for visiting us here.
