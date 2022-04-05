9657eebdfda7506fe541270a1251eb4b557f0d71
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-05 16:31:25 +0600

    not found image added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import image from './images/resultNotFound.gif';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Result Not found</h1>
-            <img width={800} height={400} src="./images/resultNotFound.gif" alt="" />
+        <div className='text-center'>
+            <h1 className='my-5'>Result Not found</h1>
+            <img width={800} height={400} src={image} alt="" />
         </div>
     );
 };
