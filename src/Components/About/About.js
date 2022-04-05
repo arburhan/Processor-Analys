a4196237a3fc906d2dc21b8413956902823558d7
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-05 16:14:12 +0600

    about us done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,11 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>About page</h2>
+            <h1 className='text-center my-5' style={{ color: 'blueviolet' }}>Few line about us</h1>
+            <h3 className='px-5'>This is the Processor Analysis website. We have tried our best to give a review of the best, best quality and fastest processor in the market. In this case we have chosen the processor of amd.
+                We may not match your preferences. Don't be sad.
+                Thank you for visiting us here.
+                Request for advice: <a href=""></a> </h3>
         </div>
     );
 };
